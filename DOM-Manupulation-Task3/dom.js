db8aac178a9f7bc149f9dc14b28b9dfb139c8066
Tushar-92 <ts.aerotab@gmail.com>
2022-10-09 20:36:39 +0530

    Task-6: Deliverable 2 i.e using querySelectorAll

--- DOM-Manupulation-Task3/dom.js
+++ DOM-Manupulation-Task3/dom.js
@@ -1,9 +1,11 @@
 // Task-6
-// querySelector //
+// querySelectorAll //
 
-let seconditem = document.querySelector('.list-group-item:nth-child(2)');
-seconditem.style.backgroundColor = "green";
+let items = document.querySelectorAll('li');
+items[1].style.color = "green";
 
-let thirditem = document.querySelector(".list-group-item:nth-child(3)");
-thirditem.style.visibility = "hidden";
-
+let odd = document.querySelectorAll('li:nth-child(odd)');
+for(let i=0;i<odd.length;i++)
+{
+    odd[i].style.backgroundColor = "Green";
+}
