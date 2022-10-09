af28dbbd02a2995122b547463daf04fa5a92c5e9
Tushar-92 <ts.aerotab@gmail.com>
2022-10-09 16:21:25 +0530

    Task4 - Edited list items using getElementsByClass

--- DOM-Manupulation-Task3/dom.js
+++ DOM-Manupulation-Task3/dom.js
@@ -4,3 +4,11 @@ headerTitle.style.borderBottom = "solid 4px #000 ";
 let AddItem = document.getElementsByClassName("title");
 AddItem[0].style.color = "Green";
 AddItem[0].style.fontWeight = "bold";
+
+let item = document.getElementsByClassName('list-group-item');
+item[2].style.backgroundColor = "Green";
+
+for(let i=0;i<item.length;i++)
+{
+    item[i].style.fontWeight = "bold";
+}
