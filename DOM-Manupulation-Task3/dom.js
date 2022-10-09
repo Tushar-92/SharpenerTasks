80b77f9fc65e0ef5751785a96d84080b08a55297
Tushar-92 <ts.aerotab@gmail.com>
2022-10-09 11:02:00 +0530

    first comit

---
+++ DOM-Manupulation-Task3/dom.js
@@ -0,0 +1,6 @@
+let headerTitle = document.getElementById("header-title");
+headerTitle.style.borderBottom = "solid 4px #000 ";
+
+let AddItem = document.getElementsByClassName("title");
+AddItem[0].style.color = "Green";
+AddItem[0].style.fontWeight = "bold";
