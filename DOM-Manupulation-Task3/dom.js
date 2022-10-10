1b6f1a353bd73456c14f4a2d8dcd1261335a48f9
Tushar-92 <ts.aerotab@gmail.com>
2022-10-10 09:23:43 +0530

    Task-7

--- DOM-Manupulation-Task3/dom.js
+++ DOM-Manupulation-Task3/dom.js
@@ -1,11 +1,52 @@
-// Task-6
-// querySelectorAll //
 
-let items = document.querySelectorAll('li');
-items[1].style.color = "green";
+// Task-7
 
-let odd = document.querySelectorAll('li:nth-child(odd)');
-for(let i=0;i<odd.length;i++)
-{
-    odd[i].style.backgroundColor = "Green";
-}
+let itemList = document.querySelector('#items');
+
+//parentElement
+console.log(itemList.parentElement);
+itemList.parentElement.style.backgroundColor = '#f4f4f4';
+console.log(itemList.parentElement.parentElement);
+
+//childNodes
+console.log(itemList.childNodes);
+
+//childrem
+console.log(itemList.children);
+console.log(itemList.children[0]);
+itemList.children[1].style.backgroundColor = 'Yellow';
+
+//firstElementChild
+itemList.firstElementChild.textContent = 'Hello 1';
+
+//lastElementChild
+itemList.lastElementChild.textContent = 'Hello 2';
+
+//nextSibling
+console.log(itemList.nextSibling);
+
+//nextElementSibling
+console.log(itemList.nextElementSibling);
+
+//previousSibling
+console.log(itemList.previousSibling);
+
+//previousElementSibling
+console.log(itemList.previousElementSibling);
+itemList.previousElementSibling.style.color = "Red";
+
+/////////////Creating Element///////////////////////
+let newDiv = document.createElement('div');
+newDiv.className = 'hello';
+newDiv.id = 'hello1';
+newDiv.setAttribute ('title' , 'Hello Div');
+
+let newDivText = document.createTextNode('Hello World');
+
+newDiv.appendChild(newDivText);
+
+console.log(newDiv);
+
+let container = document.querySelector('header .container');
+let h1 = document.querySelector('header h1');
+container.insertBefore(newDiv, h1); 
