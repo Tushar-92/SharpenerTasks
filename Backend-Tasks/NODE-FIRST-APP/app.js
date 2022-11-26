1442bd430fc2cc4709a7e41251f124d54c075210
Tushar-92 <ts.aerotab@gmail.com>
2022-11-26 21:48:41 +0530

    Starting with Express Framework - Task 2 - Code with the 2 middlewares and app.listen as shown by the youtuber

--- Backend-Tasks/NODE-FIRST-APP/app.js
+++ Backend-Tasks/NODE-FIRST-APP/app.js
@@ -1,50 +1,19 @@
 
 const http = require('http');
 
-const server = http.createServer((req, res) => {
-    
-    //Just for Analysis
-        // console.log(req.url);
-        // console.log(req.method);
-        // console.log(req.headers);
-        // process.exit();
+const express = require('express');
 
-    //Handling the request
-        //First let's Handle the url request
+const app = express();
 
-    if(req.url == '/') {
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title>My First Page</title></head>');
-        res.write('<body>Hello from my Node.js Server!</h1></body>')
-        res.write('</html>');
-        res.end();
-    }
-    else if(req.url == '/home') {
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title>My First Page</title></head>');
-        res.write('<body>Welcome home</h1></body>')
-        res.write('</html>');
-        res.end();
-    }
-    else if(req.url == '/about') {
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title>My First Page</title></head>');
-        res.write('<body>Welcome to About Us page</h1></body>')
-        res.write('</html>');
-        res.end();
-    }
-    else if(req.url == '/node') {
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title>My First Page</title></head>');
-        res.write('<body>Welcome to my Node Js project</h1></body>')
-        res.write('</html>');
-        res.end();
-        }
+app.use((req , res , next) => {
+    console.log('In the middleware!'); 
+    next(); // this will allows the request to continue to the next middleware in line
 });
 
-server.listen(4000);
+app.use((req , res , next) => {
+    console.log('In the another middleware!'); 
+    res.send('<h1>Hello from Express!</h1>');
+});
 
+
+app.listen(3000);
