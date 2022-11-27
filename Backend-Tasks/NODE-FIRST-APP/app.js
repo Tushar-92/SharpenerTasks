f6a54267f31fe103ee7ac9e01ec56fdc3e93a528
Tushar-92 <ts.aerotab@gmail.com>
2022-11-27 11:24:46 +0530

    Getting Started with Express - Task 3

--- Backend-Tasks/NODE-FIRST-APP/app.js
+++ Backend-Tasks/NODE-FIRST-APP/app.js
@@ -3,15 +3,22 @@ const http = require('http');
 
 const express = require('express');
 
+const bodyParser = require('body-parser');
+
 const app = express();
 
-app.use((req , res , next) => {
-    console.log('In the middleware!'); 
-    next(); // this will allows the request to continue to the next middleware in line
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product' , (req , res , next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button></form>');
 });
 
-app.use((req , res , next) => {
-    console.log('In the another middleware!'); 
+app.post('/product' , (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/' , (req , res , next) => {
     res.send('<h1>Hello from Express!</h1>');
 });
 
